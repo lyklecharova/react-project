d859df1e46f84c01875d3019923326796d006498
lyklecharova <lklecharova9@gmail.com>
2023-11-14 11:26:02 +0200

    Update client folder
    Add path recipes create
    Add path recipes id

--- fe/client/src/App.jsx
+++ fe/client/src/App.jsx
@@ -7,6 +7,8 @@ import { Login } from './components/Login/Login';
 import { Home } from './components/Home/Home';
 import { Dashboard } from './components/Dashboard/Dashboard';
 import {ProfileDetails} from './components/ProfileDetails/ProfileDetails';
+import { CreateRecipe } from "./components/CreateRecipe/CreateRecipe";
+import { DetailsRecipes } from "./components/DetailsRecipes/DetailsRecipes";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard/*" element={<Dashboard  />} />
+          <Route path="/recipes/create" element={<CreateRecipe />} />
+          <Route path="/recipes/:id" element={<DetailsRecipes /> } />
           <Route path="/profile" element={<ProfileDetails />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
