abbea4805d198c514c49e20456651a72cf542919
lyklecharova <lklecharova9@gmail.com>
2023-11-14 11:20:42 +0200

    mapping dashboardItem

--- fe/client/src/components/Dashboard/Dashboard.jsx
+++ fe/client/src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from 'react';
+import * as recipeService from '../../services/recipeService';
 import styles from "./Dashboard.module.css";
-import {DashboardItem} from './DashboardItem/DashboardItem' 
-export const Dashboard = ({
-  recipes
-}) => {
+import { DashboardItem } from './DashboardItem/DashboardItem';
+
+export const Dashboard = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    recipeService.getAll()
+      .then(result => setRecipes(result))
+  }, []);
+
   return (
     <div className={styles["container-dashboard"]}>
       <div className={styles["container-dashboard-list"]}>
-    	  {recipes.map(x=> <DashboardItem key={x.id} {...x}/>)}
+      {recipes.map(recipe => (
+          <DashboardItem key={recipe._id} {...recipe} />
+        ))}
       </div>
     </div>
   );
