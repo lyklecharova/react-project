4ae1f895b5d02d17536e062473b57f952b50138d
lyklecharova <lklecharova9@gmail.com>
2023-11-16 11:10:39 +0200

    Update recipeService.js
    remove whitespace

--- fe/client/src/services/recipeService.js
+++ fe/client/src/services/recipeService.js
@@ -3,8 +3,15 @@ import * as request from '../lib/request';
 const baseUrl = 'http://localhost:3030/jsonstore/recipes';
 
 export const getAll = async() =>{
-    const result = await request.get( baseUrl);
+    const result = await request.get(baseUrl);
     return Object.values(result);
+};
+
+export const  getOne = async(recipeId) =>{
+    const result = await request.get(`${baseUrl}/${recipeId}`, );
+
+
+    return result;
 };
 
 export const create = async (recipeData) =>{
