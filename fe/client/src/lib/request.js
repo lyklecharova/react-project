7b1a59571b31cd377b571c2027cac90c7a25c8a6
lyklecharova <lklecharova9@gmail.com>
2023-11-16 09:59:42 +0200

    Update request.js

--- fe/client/src/lib/request.js
+++ fe/client/src/lib/request.js
@@ -1,8 +1,27 @@
-export const request = async(method, url, data) =>{
+const recipeOptions = (data) =>{
+    const options = {};
+    if(data){
+        options.body = JSON.stringify(data);
+        options.header = {
+            'content-type': 'application/json'
+        };
+    }
+    return options;
+};
+const request = async(method, url, data) =>{
     const response = await fetch(url, {
+        ...recipeOptions(data),
         method,
     });
    
     const result = await response.json();
     return result;
 };
+
+export const get = request.bind(null, 'GET');
+export const post = request.bind(null, 'POST');
+export const put = request.bind(null, 'PUT');
+export const patch = request.bind(null, 'PATCH');
+export const del = request.bind(null, 'DELETE');
+
+
