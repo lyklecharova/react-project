edf092171f34b25a07e96e37c1c594ab109d39e5
lyklecharova <lklecharova9@gmail.com>
2023-11-22 09:08:44 +0200

    update request.js

--- fe/client/src/lib/request.js
+++ fe/client/src/lib/request.js
@@ -1,20 +1,37 @@
-const recipeOptions = (data) =>{
+const recipeOptions = (data) => {
     const options = {};
-    if(data){
+    if (data) {
         options.body = JSON.stringify(data);
         options.headers = {
             'content-type': 'application/json'
         };
     }
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+        options.headers = {
+            ...options.headers, // добавяне на нови или променяне на съществуващи свойства
+            'X-Authorization': token
+        };
+    }
+
     return options;
 };
-const request = async(method, url, data) =>{
+const request = async (method, url, data) => {
     const response = await fetch(url, {
         ...recipeOptions(data),
         method,
     });
-   
+
+    if (response.status === 204) {
+        // status 204 === NO CONTENT
+        return {};
+    }
+
     const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    }
     return result;
 };
 
