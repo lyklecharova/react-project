26c272d73b447f1e822a0e04791c9b7aec43d992
lyklecharova <lklecharova9@gmail.com>
2023-11-22 12:47:37 +0200

    Update Header.jsx
    Change Header

--- fe/client/src/components/Header/Header.jsx
+++ fe/client/src/components/Header/Header.jsx
@@ -1,71 +1,78 @@
-import styles from './Header.module.css'
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { NavLink } from "react-router-dom";
 
+import styles from './Header.module.css'
+import { AuthContext } from '../../contexts/authContext';
+
 export const Header = () => {
+    const { isAuthenticated, username } = useContext(AuthContext);
     return (
         <header className={styles['header']}>
             <nav className={styles['nav']}>
-                
                 <Link to="/">LK</Link>
                 <ul role="list" className={styles['list']}>
                     <li className={styles['element']}>
-                    <NavLink style={({ isActive }) => ({
-                        color: isActive ? "#FF8C00" : "#F5DEB3",
-                    })} to="/">
-                        Home
-                    </NavLink>
+                        <NavLink style={({ isActive }) => ({
+                            color: isActive ? "#FF8C00" : "#F5DEB3"
+                        })} to="/">
+                            Home
+                        </NavLink>
                     </li>
                     <li className={styles['element']}>
-                    <NavLink className={styles['element']} style={({ isActive }) => ({
-                        color: isActive ? "#FF8C00" : "#F5DEB3",
-                    })} to="/dashboard">
-                        Dashboard
-                    </NavLink>
+                        <NavLink className={styles['element']} style={({ isActive }) => ({
+                            color: isActive ? "#FF8C00" : "#F5DEB3"
+                        })} to="/dashboard">
+                            Dashboard
+                        </NavLink>
                     </li>
-                    <li className={styles['element']}>
-                    <NavLink className={styles['element']} style={({ isActive }) => ({
-                        color: isActive ? "#FF8C00" : "#F5DEB3",
-                    })} to="/recipes/create">
-                        Create Recipes
-                    </NavLink>
-                    </li>
-                    <li className={styles['element']}>
-                    <NavLink className={styles['element']} style={({ isActive }) => ({
-                        color: isActive ? "#FF8C00" : "#F5DEB3",
-                    })} to="/profile">
-                        Profile
-                    </NavLink>
-                    </li>
-                    <li className={styles['element']}>
-                    <NavLink className={styles['element']} style={({ isActive }) => ({
-                        color: isActive ? "#FF8C00" : "#F5DEB3",
-                    })} to="/login">
-                        Login
-                    </NavLink>
-                    </li>
-                    <li className={styles['element']}>
-                    <NavLink className={styles['element']} style={({ isActive }) => ({
-                        color: isActive ? "#FF8C00" : "#F5DEB3",
-                    })} to="/register">
-                        Register
-                    </NavLink>
-                    </li>
-                    <li className={styles['element']}>
-                    <NavLink className={styles['element']} style={({ isActive }) => ({
-                        color: isActive ? "#FF8C00" : "#F5DEB3",
-                    })} to="/logout">
-                        Logout
-                    </NavLink>
-                    </li>
+                    {isAuthenticated && (
+                        <>
+                            <li className={styles['element']}>
+                                <NavLink className={styles['element']} style={({ isActive }) => ({
+                                    color: isActive ? "#FF8C00" : "#F5DEB3"
+                                })} to="/recipes/create">
+                                    Create Recipes
+                                </NavLink>
+                            </li>
+                            <li className={styles['element']}>
+                                <NavLink className={styles['element']} style={({ isActive }) => ({
+                                    color: isActive ? "#FF8C00" : "#F5DEB3"
+                                })} to="/profile">
+                                    Profile
+                                </NavLink>
+                            </li>
+                            <li className={styles['element']}>
+                                <NavLink className={styles['element']} style={({ isActive }) => ({
+                                    color: isActive ? "#FF8C00" : "#F5DEB3"
+                                })} to="/logout">
+                                    Logout
+                                </NavLink>
+                            </li>
+                            <span className={styles['user']}>{`Wellcome, ${username}`}</span>
+                        </>
+                    )}
 
-
-
+                    {!isAuthenticated && (
+                        <>
+                            <li className={styles['element']}>
+                                <NavLink className={styles['element']} style={({ isActive }) => ({
+                                    color: isActive ? "#FF8C00" : "#F5DEB3"
+                                })} to="/login">
+                                    Login
+                                </NavLink>
+                            </li>
+                            <li className={styles['element']}>
+                                <NavLink className={styles['element']} style={({ isActive }) => ({
+                                    color: isActive ? "#FF8C00" : "#F5DEB3"
+                                })} to="/register">
+                                    Register
+                                </NavLink>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </nav>
-
-            
-
         </header>
     );
 }
