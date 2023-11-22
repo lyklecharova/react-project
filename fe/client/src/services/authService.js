f8f36d4d0613aa28ecee7c8fed4f34091c476bf2
lyklecharova <lklecharova9@gmail.com>
2023-11-22 09:21:32 +0200

    Add auth service

---
+++ fe/client/src/services/authService.js
@@ -0,0 +1,19 @@
+import * as request from '../lib/request';
+
+const baseUrl = 'http://localhost:3030/users';
+
+export const login = async (email, password) =>{
+    const result = await request.post(`${baseUrl}/login`, {
+        email,
+        password
+    });
+
+    return result;
+};
+
+export const register = (email, password) => request.post(`${baseUrl}/register`, {
+    email,
+    password,
+});
+
+export const logout = () => request.get(`${baseUrl}/logout`);
