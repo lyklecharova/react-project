dc2b87583a5d0e4e64391720b0ae0d8165765986
lyklecharova <lklecharova9@gmail.com>
2023-11-23 15:56:15 +0200

    Update Dashboard
    Update DashboardItem

--- fe/client/src/components/Dashboard/Dashboard.jsx
+++ fe/client/src/components/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import * as recipeService from '../../services/recipeService';
 import styles from "./Dashboard.module.css";
 import { DashboardItem } from './DashboardItem/DashboardItem';
+import { Link } from 'react-router-dom';
 
 export const Dashboard = () => {
   const [recipes, setRecipes] = useState([]);
@@ -14,9 +15,18 @@ export const Dashboard = () => {
   return (
     <div className={styles["container-dashboard"]}>
       <div className={styles["container-dashboard-list"]}>
-      {recipes.map(recipe => (
+        {recipes.map(recipe => (
           <DashboardItem key={recipe._id}{...recipe} />
         ))}
+
+        {recipes.length === 0 && (
+          <div>
+          <h2 className={styles['recipe-error']}>Can not find recipe yet!</h2>
+          <span className={styles['error']}>
+          <Link to={'/'}>Oops, back to the home</Link></span>
+          </div>
+        )}
+
       </div>
     </div>
   );
--- fe/client/src/components/Dashboard/DashboardItem/DashboardItem.jsx
+++ fe/client/src/components/Dashboard/DashboardItem/DashboardItem.jsx
@@ -1,31 +1,28 @@
 import { Link } from 'react-router-dom';
 import styles from "../Dashboard.module.css";
+
 export const DashboardItem = ({
     _id,
     image,
     title,
     description
 }) => {
-
-  const truncatedTitle = title.length > 10 ? `${title.slice(0, 10)}...` : title;
+  const truncatedTitle = title && title.length > 10 ? `${title.slice(0, 10)}...` : title;
   const truncatedDescription =
-  String(description).length > 40
-    ? `${String(description).slice(0, 40)}...`
-    : String(description);
+      description && String(description).length > 40
+          ? `${String(description).slice(0, 40)}...`
+          : String(description);
 
   return (
-    <div className={styles["dashboard-content"]}>
-      <img className={styles["dashboard-img"]} src={image} alt={truncatedTitle}/>
-      <div className={styles["dashboard-information"]}>
-        <h3 className={styles["dashboard-information-title"]}>{truncatedTitle}</h3>
-        <p className={styles["dashboard-information-description"]}>
-          {truncatedDescription}
-        </p>
+      <div className={styles["dashboard-content"]}>
+          <img className={styles["dashboard-img"]} src={image} alt={truncatedTitle} />
+          <div className={styles["dashboard-information"]}>
+              <h3 className={styles["dashboard-information-title"]}>{truncatedTitle}</h3>
+              <p className={styles["dashboard-information-description"]}>{truncatedDescription}</p>
+          </div>
+          <Link to={`/recipes/${_id}`}>
+              <button className={styles["dashboard-button"]}>Whole Recipe</button>
+          </Link>
       </div>
-
-      <Link to={`/recipes/${_id}`}>
-        <button className={styles["dashboard-button"]}>Whole Recipe</button>
-      </Link>
-    </div>
   );
 }
