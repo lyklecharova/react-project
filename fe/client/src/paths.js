db3df92ee844fa210466d658b125edbb0574a501
lyklecharova <lklecharova9@gmail.com>
2023-11-23 15:51:18 +0200

    Create paths.js

---
+++ fe/client/src/paths.js
@@ -0,0 +1,11 @@
+export const Path = {
+    Home: '/',
+    Dashboard: '/dashboard',
+    CreateRecipe: '/recipes/create',
+    DetailsRecipes: '/recipes/:recipeId',
+    ProfileDetails: '/profile',
+    Login: '/login',
+    Register: '/register',
+    Logout: '/logout',
+    PageNotFound: '/*'
+}
