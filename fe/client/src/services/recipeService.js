7498f063e02a10474f57bc4b0a27c809eacd6ab6
lyklecharova <lklecharova9@gmail.com>
2023-11-23 15:57:11 +0200

    Update recipeService

--- fe/client/src/services/recipeService.js
+++ fe/client/src/services/recipeService.js
@@ -4,18 +4,15 @@ const baseUrl = 'http://localhost:3030/data/recipes';
 
 export const getAll = async() =>{
     const result = await request.get(baseUrl);
-    return Object.values(result);
+    return result; // return arrays of objects 
 };
 
 export const  getOne = async(recipeId) =>{
-    const result = await request.get(`${baseUrl}/${recipeId}`, );
-
-
+    const result = await request.get(`${baseUrl}/${recipeId}`,);
     return result;
 };
 
 export const create = async (recipeData) =>{
     const result = await request.post(baseUrl, recipeData);
-   
     return result;
 };
