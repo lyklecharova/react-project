9b62c86c8b10e6d84667cf4ac778b89ebeab971f
lyklecharova <lklecharova9@gmail.com>
2023-11-26 22:48:20 +0200

    update App.jsx

--- fe/client/src/App.jsx
+++ fe/client/src/App.jsx
@@ -1,35 +1,44 @@
 import { Routes, Route } from "react-router-dom";
 
+import { AuthProvider } from "./contexts/authContext";
+import { Path } from "./paths";
+
 import { Header } from './components/Header/Header';
-import { Footer } from './components/Footer/Footer';
-import { Register } from './components/Register/Register';
-import { Login } from './components/Login/Login';
 import { Home } from './components/Home/Home';
 import { Dashboard } from './components/Dashboard/Dashboard';
-import {ProfileDetails} from './components/ProfileDetails/ProfileDetails';
 import { CreateRecipe } from "./components/CreateRecipe/CreateRecipe";
 import { DetailsRecipes } from "./components/DetailsRecipes/DetailsRecipes";
+import { EditRecipe } from "./components/EditRecipe/EditRecipe";
+import { ProfileDetails } from './components/ProfileDetails/ProfileDetails';
+import { PageNotFound } from "./components/PageNotFound/PageNotFound";
+import { Footer } from './components/Footer/Footer';
+
+import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
+import { Logout } from "./components/Logout/Logout";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard/*" element={<Dashboard  />} />
+          <Route path={Path.Home} element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/recipes/create" element={<CreateRecipe />} />
-          <Route path="/recipes/:recipeId" element={<DetailsRecipes /> } />
+          <Route path="/recipes/:recipeId" element={<DetailsRecipes />} />
+          <Route path={Path.RecipeEdit} element={<EditRecipe />} />
           <Route path="/profile" element={<ProfileDetails />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/logout" element={<Home />} />
+          <Route path={Path.Logout} element={<Logout />} />
+          <Route path="*" element={<PageNotFound />} />
 
         </Routes>
       </main>
 
       <Footer />
-    </>
+    </AuthProvider>
   )
 }
 export default App
