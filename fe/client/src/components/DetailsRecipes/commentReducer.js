0e3709d7154d418f3afbc7c9a1d2a83ee2a762e1
lyklecharova <lklecharova9@gmail.com>
2023-11-26 22:45:03 +0200

    create comment reducer

---
+++ fe/client/src/components/DetailsRecipes/commentReducer.js
@@ -0,0 +1,12 @@
+export const reducer = (state, action) => {
+    switch (action?.type) {
+        case 'GET_ALL_COMMENTS':
+            return [...action.payload];
+        case 'ADD_COMMENT':
+            return [...state, action.payload];
+        case 'EDIT_COMMENT':
+            return state.map(c => c._id === action.payload._id ? { ...c, text: action.payload.text } : c)
+        default:
+            return state;
+    }
+}
