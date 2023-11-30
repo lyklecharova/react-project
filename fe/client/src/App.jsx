e1063a06afbc11cf7774a32796a269224a720922
lyklecharova <lklecharova9@gmail.com>
2023-11-30 18:08:18 +0200

    Update app jsx
    Added auth guard

--- fe/client/src/App.jsx
+++ fe/client/src/App.jsx
@@ -16,29 +16,37 @@ import { Footer } from './components/Footer/Footer';
 import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
 import { Logout } from "./components/Logout/Logout";
+import AuthGuard from "./components/guards/AuthGuard";
+import { EditProfile } from "./components/EditProfile/EditProfile";
 
 function App() {
   return (
+    
     <AuthProvider>
       <Header />
       <main>
         <Routes>
           <Route path={Path.Home} element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/recipes/create" element={<CreateRecipe />} />
           <Route path="/recipes/:recipeId" element={<DetailsRecipes />} />
-          <Route path={Path.RecipeEdit} element={<EditRecipe />} />
-          <Route path="/profile" element={<ProfileDetails />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path={Path.Logout} element={<Logout />} />
           <Route path="*" element={<PageNotFound />} />
+
+          <Route element={<AuthGuard />}>
+            <Route path="/recipes/create" element={<CreateRecipe />} />
+            <Route path="/profile" element={<ProfileDetails />} />
+            <Route path={Path.ProfileEdit} element={<EditProfile />} />
+            <Route path={Path.RecipeEdit} element={<EditRecipe />} />
+            <Route path={Path.Logout} element={<Logout />} />
+          </Route>
 
         </Routes>
       </main>
 
       <Footer />
     </AuthProvider>
+ 
   )
 }
 export default App
