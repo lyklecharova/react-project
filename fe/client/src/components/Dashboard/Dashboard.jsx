83fee48208fe8f9a3a2be1a2e885b47c4b222a1e
lyklecharova <lklecharova9@gmail.com>
2023-12-09 11:42:43 +0200

    added pagination

--- fe/client/src/components/Dashboard/Dashboard.jsx
+++ fe/client/src/components/Dashboard/Dashboard.jsx
@@ -6,28 +6,51 @@ import { Link } from 'react-router-dom';
 
 export const Dashboard = () => {
   const [recipes, setRecipes] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
 
   useEffect(() => {
     recipeService.getAll()
       .then(result => setRecipes(result))
   }, []);
 
+  // Разделяне на списъка с рецепти на страници
+  const indexOfLastRecipe = currentPage * itemsPerPage;
+  const indexOfFirstRecipe = indexOfLastRecipe - itemsPerPage;
+  const currentRecipes = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
+
+  // Промяна на страницата
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className={styles["container-dashboard"]}>
       <div className={styles["container-dashboard-list"]}>
-        {recipes.map(recipe => (
-          <DashboardItem key={recipe._id}{...recipe} />
+        {currentRecipes.map(recipe => (
+          <DashboardItem key={recipe._id} {...recipe} />
         ))}
 
         {recipes.length === 0 && (
           <div>
-          <h2 className={styles['recipe-error']}>Can not find recipe yet!</h2>
-          <span className={styles['error']}>
-          <Link to={'/'}>Oops, back to the home</Link></span>
+            <h2 className={styles['recipe-error']}>Can not find recipe yet!</h2>
+            <span className={styles['error']}>
+              <Link to={'/'}>Oops, back to the home</Link>
+            </span>
           </div>
         )}
 
+        <div className={styles['pagination']}>
+          {Array.from({ length: Math.ceil(recipes.length / itemsPerPage) }, (_, index) => (
+            // Първият параметър, представен с _, се игнорира, тъй като не ни е необходим.
+            // Вторият параметър, index, представлява индекса на текущия елемент в масива, който използваме като ключ за бутона.
+            <button key={index} onClick={() => paginate(index + 1)}>
+              {index + 1}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
+
