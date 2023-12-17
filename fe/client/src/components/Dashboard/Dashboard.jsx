65aef115cba6014f59151cbc0d0e36098ce8a384
lyklecharova <lklecharova9@gmail.com>
2023-12-17 09:10:48 +0200

    update
    update dashboard

--- fe/client/src/components/Dashboard/Dashboard.jsx
+++ fe/client/src/components/Dashboard/Dashboard.jsx
@@ -2,55 +2,70 @@ import { useEffect, useState } from 'react';
 import * as recipeService from '../../services/recipeService';
 import styles from "./Dashboard.module.css";
 import { DashboardItem } from './DashboardItem/DashboardItem';
-import { Link } from 'react-router-dom';
+
+
+import { SearchBar } from '../Search/SearchBar';
+import { paginationIndex } from '../../utils/paginationIndex';
+
 
 export const Dashboard = () => {
   const [recipes, setRecipes] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 4;
+  const [foundRecipes, setFoundRecipes] = useState([]);
+  const [currentRecipes, setCurrentRecipes] = useState([]);
+  const { itemsPerPage } = paginationIndex();
 
   useEffect(() => {
     recipeService.getAll()
-      .then(result => setRecipes(result))
+      .then(result => {
+        setRecipes(result);
+        const { indexOfFirstRecipe, indexOfLastRecipe } = paginationIndex(1);
+        setCurrentRecipes(result.slice(indexOfFirstRecipe, indexOfLastRecipe))
+      })
   }, []);
-
-  // Разделяне на списъка с рецепти на страници
-  const indexOfLastRecipe = currentPage * itemsPerPage;
-  const indexOfFirstRecipe = indexOfLastRecipe - itemsPerPage;
-  const currentRecipes = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
 
   // Промяна на страницата
   const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber);
+    const paginatedRecipe = foundRecipes.length === 0 ? recipes : foundRecipes;
+    const { indexOfFirstRecipe, indexOfLastRecipe } = paginationIndex(pageNumber)
+    setCurrentRecipes(paginatedRecipe.slice(indexOfFirstRecipe, indexOfLastRecipe))
   };
 
+  const onSearchRecipe = (foundRecipes) => {
+    setFoundRecipes(foundRecipes);
+    const { indexOfFirstRecipe, indexOfLastRecipe } = paginationIndex(1);
+    setCurrentRecipes(foundRecipes.slice(indexOfFirstRecipe, indexOfLastRecipe))
+
+  }
   return (
     <div className={styles["container-dashboard"]}>
+      <SearchBar onSearchRecipe={onSearchRecipe} recipes={recipes} />
       <div className={styles["container-dashboard-list"]}>
         {currentRecipes.map(recipe => (
           <DashboardItem key={recipe._id} {...recipe} />
         ))}
 
-        {recipes.length === 0 && (
+        {currentRecipes.length === 0 && (
           <div>
             <h2 className={styles['recipe-error']}>Can not find recipe yet!</h2>
-            <span className={styles['error']}>
-              <Link to={'/'}>Oops, back to the home</Link>
-            </span>
           </div>
         )}
 
-        <div className={styles['pagination']}>
-          {Array.from({ length: Math.ceil(recipes.length / itemsPerPage) }, (_, index) => (
-            // Първият параметър, представен с _, се игнорира, тъй като не ни е необходим.
-            // Вторият параметър, index, представлява индекса на текущия елемент в масива, който използваме като ключ за бутона.
-            <button key={index} onClick={() => paginate(index + 1)}>
-              {index + 1}
-            </button>
-          ))}
-        </div>
+        {currentRecipes.length !== 0 && (
+          <div className={styles['pagination']}>
+
+            {Array.from({ length: Math.ceil((foundRecipes.length !== 0 ? foundRecipes.length : recipes.length) / itemsPerPage) }, (_, index) => (
+              // Първият параметър, представен с _, се игнорира, тъй като не ни е необходим.
+              // Вторият параметър, index, представлява индекса на текущия елемент в масива, който използваме като ключ за бутона.
+              <button key={index} onClick={() => paginate(index + 1)}>
+                {index + 1}
+              </button>
+            ))}
+          </div>
+
+        )}
       </div>
     </div>
   );
 };
+
 
--- fe/client/src/components/Dashboard/DashboardItem/DashboardItem.jsx
+++ fe/client/src/components/Dashboard/DashboardItem/DashboardItem.jsx
@@ -7,22 +7,22 @@ export const DashboardItem = ({
     title,
     description
 }) => {
-  const truncatedTitle = title && title.length > 10 ? `${title.slice(0, 10)}...` : title;
-  const truncatedDescription =
-      description && String(description).length > 40
-          ? `${String(description).slice(0, 40)}...`
-          : String(description);
+    const truncatedTitle = title && title.length > 10 ? `${title.slice(0, 10)}...` : title;
+    const truncatedDescription =
+        description && String(description).length > 40
+            ? `${String(description).slice(0, 40)}...`
+            : String(description);
 
-  return (
-      <div className={styles["dashboard-content"]}>
-          <img className={styles["dashboard-img"]} src={image} alt={truncatedTitle} />
-          <div className={styles["dashboard-information"]}>
-              <h3 className={styles["dashboard-information-title"]}>{truncatedTitle}</h3>
-              <p className={styles["dashboard-information-description"]}>{truncatedDescription}</p>
-          </div>
-          <Link to={`/recipes/${_id}`}>
-              <button className={styles["dashboard-button"]}>Whole Recipe</button>
-          </Link>
-      </div>
-  );
+    return (
+        <div className={styles["dashboard-content"]}>
+            <img className={styles["dashboard-img"]} src={image} alt={truncatedTitle} />
+            <div className={styles["dashboard-information"]}>
+                <h3 className={styles["dashboard-information-title"]}>{truncatedTitle}</h3>
+                <p className={styles["dashboard-information-description"]}>{truncatedDescription}</p>
+            </div>
+            <Link className={styles['dashboard-link']} to={`/recipes/${_id}`}>
+                <button className={styles["dashboard-button"]}>Whole Recipe</button>
+            </Link>
+        </div>
+    );
 }
